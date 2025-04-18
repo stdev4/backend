4612b64a125de0c21369af0ff783c0962219f6d3
making-a-scene <4936joo@naver.com>
2025-04-18 23:46:52 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/example/stdev_hack/StdevHackApplication.java
@@ -0,0 +1,13 @@
+package com.example.stdev_hack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdevHackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StdevHackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stdev_hack/StdevHackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stdev_hack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdevHackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
