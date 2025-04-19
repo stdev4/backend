ade181a060b094f80e7de28a8a2f331b9951472e
making-a-scene <4936joo@naver.com>
2025-04-19 15:59:17 +0900

    feat: UserController 생성

---
+++ src/main/java/com/example/stdev_hack/domain/user/UserController.java
@@ -0,0 +1,13 @@
+package com.example.stdev_hack.domain.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+}
