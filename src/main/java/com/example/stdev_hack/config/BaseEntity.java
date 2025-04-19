5378e7c7c056fc2e7dc78ba2ad5b7b9cec0a2936
making-a-scene <4936joo@naver.com>
2025-04-19 12:51:31 +0900

    feat: BaseEntity 생성

---
+++ src/main/java/com/example/stdev_hack/config/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.example.stdev_hack.config;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class BaseEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column
+    private LocalDateTime updatedAt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+}
