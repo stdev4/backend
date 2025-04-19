907ba0b9176429acd25fc95887bdbf6fe5e9cbae
making-a-scene <4936joo@naver.com>
2025-04-19 13:10:27 +0900

    feat: UserRepository 생성

---
+++ src/main/java/com/example/stdev_hack/domain/user/UserRepository.java
@@ -0,0 +1,6 @@
+package com.example.stdev_hack.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
