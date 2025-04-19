103a7ffdb8682f1beb477fd97aa28da4fb8a5a7a
making-a-scene <4936joo@naver.com>
2025-04-19 22:45:11 +0900

    fix: 오늘의 퀴즈 조회 API GET mapping으로 변경

--- src/main/java/com/example/stdev_hack/domain/Quiz/QuizController.java
+++ src/main/java/com/example/stdev_hack/domain/Quiz/QuizController.java
@@ -4,6 +4,7 @@ import com.example.stdev_hack.dtos.CommonResponse;
 import com.example.stdev_hack.dtos.QuizResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuizController {
     private final QuizService quizService;
 
-    @RequestMapping("/daily")
+    @GetMapping("/daily")
     public ResponseEntity<CommonResponse<QuizResponse>> getDailyQuiz() {
         return ResponseEntity.ok(
                 CommonResponse.<QuizResponse>builder()
