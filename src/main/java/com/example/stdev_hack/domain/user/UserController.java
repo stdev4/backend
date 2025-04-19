39d54aeac7c4d76c5f0af0a86414661f30ec0bea
making-a-scene <4936joo@naver.com>
2025-04-19 16:08:47 +0900

    feat: 닉네임 중복 검사 API 구현

--- src/main/java/com/example/stdev_hack/domain/user/UserController.java
+++ src/main/java/com/example/stdev_hack/domain/user/UserController.java
@@ -1,6 +1,10 @@
 package com.example.stdev_hack.domain.user;
 
+import com.example.stdev_hack.dtos.CommonResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
 
+    @GetMapping("/onboarding/nickname/{nickname}/exists")
+    public ResponseEntity<CommonResponse<Boolean>> checkNicknameExistence(@PathVariable String nickname) {
+        return ResponseEntity.ok(
+                CommonResponse.<Boolean>builder()
+                        .status(200)
+                        .message("Success")
+                        .data(userService.isNicknameExist(nickname))
+                        .build()
+        );
+    }
 }
