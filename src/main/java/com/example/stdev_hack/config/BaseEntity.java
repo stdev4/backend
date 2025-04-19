e24354b728f7447d02641165bda8cb69b5a732c4
making-a-scene <4936joo@naver.com>
2025-04-19 22:09:42 +0900

    feat: QuizRepository 생성

--- src/main/java/com/example/stdev_hack/config/BaseEntity.java
+++ src/main/java/com/example/stdev_hack/config/BaseEntity.java
@@ -3,7 +3,6 @@ package com.example.stdev_hack.config;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
