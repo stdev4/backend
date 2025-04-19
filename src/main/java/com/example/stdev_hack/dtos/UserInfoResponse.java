2726b556986e1ffdd878651234b10b7d3acb4868
making-a-scene <4936joo@naver.com>
2025-04-19 16:34:21 +0900

    feat: 유저 정보 응답을 위한 dto 생성

---
+++ src/main/java/com/example/stdev_hack/dtos/UserInfoResponse.java
@@ -0,0 +1,20 @@
+package com.example.stdev_hack.dtos;
+
+import com.example.stdev_hack.domain.Field;
+import com.example.stdev_hack.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Set;
+
+@Getter
+@AllArgsConstructor
+public class UserInfoResponse {
+    private Long userId;
+    private String nickname;
+    private Set<Field> interests;
+
+    public static UserInfoResponse of(User user) {
+        return new UserInfoResponse(user.getId(), user.getNickname(), user.getInterests());
+    }
+}
