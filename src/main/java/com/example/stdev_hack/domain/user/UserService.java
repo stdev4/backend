a5fc901b860496475a97325716dd83b95273b5af
making-a-scene <4936joo@naver.com>
2025-04-19 13:12:52 +0900

    feat: UserService 생성

---
+++ src/main/java/com/example/stdev_hack/domain/user/UserService.java
@@ -0,0 +1,12 @@
+package com.example.stdev_hack.domain.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+
+}
